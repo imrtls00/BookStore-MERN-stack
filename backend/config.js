ce754e83f51ab8d7335dc2353928a229fa5a7e87
Sameer Faisal <imrtls00@gmail.com>
2024-01-12 14:26:31 +0500

    Update config.js
    Revoked and renamed MongoDB Credential

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://root:root@book-store-mern.vd8doke.mongodb.net/books-collection?retryWrites=true&w=majority';
+    '[your-monogoDB-URL-here]';
